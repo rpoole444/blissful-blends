501f82a1df58ae53da6e67e2807832022af5f661
Reid Poole <poole.reid@gmail.com>
2023-04-08 14:42:24 -0500

    Add Header icon, buttons and title

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,1 +1,22 @@
 import React from "react";
+import Logo from "/Users/reidpoole/blissful-blends/src/assets/cannabis.png"
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <section className="header-content">
+        <img className="logo" src={Logo} alt="hand and leaf" />
+        <section className="title-container">
+          <h1 className="title">Blissful Blends</h1>
+        </section>
+        <section className="button-container">
+          <button className="header-buttons">All Strains</button>
+          <button className="header-buttons">Matches</button>
+        </section>
+      </section>
+    </header>
+  )
+}
+
+export default Header
--- src/Components/Welcome/Welcome.tsx
+++ src/Components/Welcome/Welcome.tsx
@@ -1,48 +1,61 @@
 import React, {useState, useEffect} from "react";
-
+import "./Welcome.css"
+import Header from "../Header/Header";
 
 
 const Welcome = () =>{
   const [allStrains, setAllStrains] = useState([])
   const [ fetching, setFetch] = useState(false)
 
-//   const fetchData = () => {
-//      const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY || '' ,
-// 		'X-RapidAPI-Host': 'weed-strain1.p.rapidapi.com'
-// 	}
-// };
+  const fetchData = () => {
+     const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY || '' ,
+		'X-RapidAPI-Host': 'weed-strain1.p.rapidapi.com'
+	}
+};
 
-//  return fetch('https://weed-strain1.p.rapidapi.com/?ordering=-strain', options)
-// 	.then(response => response.json())
-// 	.then(data => setAllStrains(data))
-// 	.catch(err => console.error(err));
-//   }
+ return fetch('https://weed-strain1.p.rapidapi.com/?ordering=-strain', options)
+	.then(response => response.json())
+	.then(data => setAllStrains(data))
+	.catch(err => console.error(err));
+  }
 
-// useEffect(() => {
-//   if(!fetching){
-//     fetchData()
-//     setFetch(true)
-//     }
-// }, [allStrains, fetching])
-// console.log("allStrains: ", allStrains)
+useEffect(() => {
+  if(!fetching){
+    fetchData()
+    setFetch(true)
+    }
+}, [allStrains, fetching])
+console.log("allStrains: ", allStrains)
   return (
-    <body className="App">
-      <h1>Blissful Blends</h1>
+    <section className="App">
+      <Header />
       <section>
-        <section>
-          <div>
-            <img />
-            <section>
+        <section className="card-container">
+          <section>
+            <section className="main-page-boxs">
+              <img />   
               <h2>Hybrid</h2>
               <p>Combination Strains</p>
             </section>
-          </div>
+          </section>
+          <section className="sativa-indica-container">
+            <section className="main-page-boxs">
+                <img /> 
+                <h2>Sativa</h2>
+                <p>Combination Strains</p>
+              </section>
+              <section className="main-page-boxs">
+                <img />
+                <h2>Indica</h2>
+                <p>Combination Strains</p>
+              </section>
+          </section>
         </section>
       </section>
-    </body>
+    </section>
   );
 }
 export default Welcome
