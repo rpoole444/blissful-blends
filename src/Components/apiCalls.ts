0162a9024df11b34ed6c34ea4b62894d63f03984
Reid Poole <poole.reid@gmail.com>
2023-04-24 11:58:40 -0500

    Remove variable for URL

--- src/Components/apiCalls.ts
+++ src/Components/apiCalls.ts
@@ -1,21 +1,22 @@
 export const apiCalls = {
-  getAllStrains(){
+  getAllStrains() {
     const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY || '' ,
-      'X-RapidAPI-Host': 'weed-strain1.p.rapidapi.com'
-    }
-  };
-  
-   return fetch(`${process.env.REACT_APP_API_BASE_URL}/?ordering=-strain`, options)
-    .then(res => {
-                if (res.ok) {
-                    return res.json()
-                } else {
-                    return new Error("Trouble fetching All Strains")
-                }
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_KEY || "",
+        "X-RapidAPI-Host": "weed-strain1.p.rapidapi.com",
+      },
+    };
 
-          })
-}
-}
+    return fetch(
+      `https://weed-strain1.p.rapidapi.com/?ordering=-strain`,
+      options
+    ).then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return new Error("Trouble fetching All Strains");
+      }
+    });
+  },
+};
